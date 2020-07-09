076686aa106befb2d4a4922b0d8dc2e21051726e
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-07-09 13:28:35 +0300

    Search component

---
+++ src/components/Search/index.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+
+import style from './style.css';
+
+export const Search = ({ disabled, search }) => {
+    const [ value, setValue ] = useState('');
+
+    const onClick = () => search(value);
+
+    return (
+        <Container>
+            <Form className={style.form}>
+                <Form.Group controlId="searchField">
+                    <Form.Row>
+                        <Col>
+                            <Form.Label>Поиск</Form.Label>
+                        </Col>
+                    </Form.Row>
+                    <Form.Row>
+                        <Col xs={12} sm={8} md={9} className={style.column}>
+                            <Form.Control
+                                type="text"
+                                disabled={disabled}
+                                onChange={
+                                    (event) => setValue(event.target.value)
+                                }
+                            />
+                        </Col>
+                        <Col xs={12} sm={4} md={3} className={`${style.column} ${style['column--button']}`}>
+                            <Button variant="primary" disabled={disabled} onClick={onClick}>
+                                Выполнить поиск
+                            </Button>
+                        </Col>
+                    </Form.Row>
+                </Form.Group>
+            </Form>
+        </Container>
+    );
+};
