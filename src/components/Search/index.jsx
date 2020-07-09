0660ab483156bb005180aaec87db40962b9a0b61
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-07-09 14:21:17 +0300

    Search component: refactoring and logic update

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
@@ -6,38 +6,38 @@ import Form from 'react-bootstrap/Form';
 
 import style from './style.css';
 
-export const Search = ({ disabled, search }) => {
-    const [ value, setValue ] = useState('');
-
-    const onClick = () => search(value);
-
-    return (
-        <Container>
-            <Form className={style.form}>
-                <Form.Group controlId="searchField">
-                    <Form.Row>
-                        <Col>
-                            <Form.Label>Поиск</Form.Label>
-                        </Col>
-                    </Form.Row>
-                    <Form.Row>
-                        <Col xs={12} sm={8} md={9} className={style.column}>
-                            <Form.Control
-                                type="text"
-                                disabled={disabled}
-                                onChange={
-                                    (event) => setValue(event.target.value)
-                                }
-                            />
-                        </Col>
-                        <Col xs={12} sm={4} md={3} className={`${style.column} ${style['column--button']}`}>
-                            <Button variant="primary" disabled={disabled} onClick={onClick}>
-                                Выполнить поиск
-                            </Button>
-                        </Col>
-                    </Form.Row>
-                </Form.Group>
-            </Form>
-        </Container>
-    );
-};
+export const Search = ({
+    disabled,
+    onChange,
+    onClick,
+    value
+}) => (
+    <Container>
+        <Form className={style.form}>
+            <Form.Group controlId="searchField">
+                <Form.Row>
+                    <Col>
+                        <Form.Label>Поиск</Form.Label>
+                    </Col>
+                </Form.Row>
+                <Form.Row>
+                    <Col xs={12} sm={8} md={9} className={style.column}>
+                        <Form.Control
+                            type="text"
+                            disabled={disabled}
+                            onChange={
+                                (event) => onChange(event.target.value)
+                            }
+                            value={value}
+                        />
+                    </Col>
+                    <Col xs={12} sm={4} md={3} className={`${style.column} ${style['column--button']}`}>
+                        <Button variant="primary" disabled={disabled} onClick={onClick}>
+                            Выполнить поиск
+                        </Button>
+                    </Col>
+                </Form.Row>
+            </Form.Group>
+        </Form>
+    </Container>
+);
