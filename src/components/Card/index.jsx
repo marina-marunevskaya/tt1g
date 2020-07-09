1cf5a8a7f5cf8859a6b63ff25e7fcf2562d28f60
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-07-09 15:03:08 +0300

    Card component: logic update

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -3,18 +3,22 @@ import RBCard from 'react-bootstrap/Card';
 
 import style from './style.css';
 
+const POSTER_UNAVAILABLE = 'N/A';
+
 export const Card = ({
     imageAlt,
     imageSrc,
     title
 }) => (
     <RBCard className={style.card}>
-        <RBCard.Img
-            variant="top"
-            alt={imageAlt}
-            className={style.image}
-            src={imageSrc}
-        />
+        {(imageSrc && imageSrc !== POSTER_UNAVAILABLE) && (
+            <RBCard.Img
+                variant="top"
+                alt={imageAlt}
+                className={style.image}
+                src={imageSrc}
+            />
+        )}
         <RBCard.Body>
             <RBCard.Title>{title}</RBCard.Title>
         </RBCard.Body>
