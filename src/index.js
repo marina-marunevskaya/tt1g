b9af09dfe5a3112ab6a7a40588c09db32bbe52a8
Marina Marunevskaya <marina.marunevskaya@gmail.com>
2020-07-09 09:46:01 +0300

    Initial setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const title = 'A test task project';
+ 
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const webpack = require('webpack');
+
+module.exports = {
+    devServer: {
+        contentBase: './dist',
+        hot: true
+    },
+    entry: './src/index.js',
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.(js|jsx)$/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    output: {
+        filename: 'bundle.js',
+        path: __dirname + '/dist',
+        publicPath: '/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    }
+};
